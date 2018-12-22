34d6b9e35053131c00641dd04be55886ea3a6a5d
bazola <bazzybazz@gmail.com>
2018-12-22 11:25:19 -0600

    Initial commit

---
+++ desktop/src/com/bazola/roomsworld/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.bazola.roomsworld.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.bazola.roomsworld.LibGDXGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new LibGDXGame(), config);
+	}
+}
